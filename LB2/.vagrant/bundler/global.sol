767dc2ae50844eb749e57713612b76beb08ab52a
slowloris-coding <65670619+slowloris-coding@users.noreply.github.com>
2022-03-24 20:53:14 +0100

    vagrant init

---
+++ LB2/.vagrant/bundler/global.sol
@@ -0,0 +1,1 @@
+{"dependencies":[["racc",["~> 1.4"]],["nokogiri",["~> 1.6"]],["iniparse",["> 1.0"]],["log4r",["~> 1.1"]],["concurrent-ruby",["~> 1.0"]],["i18n",["~> 1.8"]],["vagrant-vmware-esxi",["= 2.5.3"]]],"checksum":"5bd2e9b26303ab4713a26732ba986266dc4f65cc3cdad6170670c8ecee6d1e23","vagrant_version":"2.2.19"}
